0a6ca8227e86b3c034756d8fbd5d07bcba587a2a
liza <gorbunova.elizaveta@gmail.com>
2019-12-27 15:28:09 +0200

    All Labs Done

--- Exercises/3-wrapper.js
+++ Exercises/3-wrapper.js
@@ -1,21 +1,19 @@
 'use strict';
 
-const contract = (fn, ...types) => {
-  return (...args) => {
-    let res = 0;
-    for (let i = 0; i <= args.length; i++) {
-      if (i == args.length) {
-        res = fn(...args);
-        if (typeof res !== typeof types[i](0)) {
-          throw new TypeError('wrong type of result');
-        }
-        return res;
+const contract = (fn, ...types) => (...args) => {
+  let res = 0;
+  for (let i = 0; i <= args.length; i++) {
+    if (i === args.length) {
+      res = fn(...args);
+      if (typeof res !== typeof types[i](0)) {
+        throw new TypeError('wrong type of result');
       }
-      if (typeof args[i] !== typeof types[i](0)) {
-        throw new TypeError('There are wrong types you used!!!');
-      }
+      return res;
     }
-  };
+    if (typeof args[i] !== typeof types[i](0)) {
+      throw new TypeError('There are wrong types you used!!!');
+    }
+  }
 };
 
 module.exports = { contract };
